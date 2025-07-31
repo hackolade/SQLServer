41fb84ac90c3a0a1e736c95d0ed78b19caf4c902
Nazar Kovtun <nazar.kovtun@hackolade.com>
2025-07-31 14:41:34 +0300

    HCK-12142: fixed scaping

--- forward_engineering/helpers/columnDefinitionHelper.js
+++ forward_engineering/helpers/columnDefinitionHelper.js
@@ -68,13 +68,11 @@ module.exports = app => {
 	 * @param {string} str
 	 * @returns {string}
 	 */
-	const escapeQuotes = str => _.trim(str).replace(/(\')+/g, "'$1");
+	const escapeQuotes = str => _.trim(str).replace(/(\')+/g, '');
 
 	const decorateDefault = (type, defaultValue) => {
-		if (isString(type) && defaultValue !== 'NULL') {
-			return `'${escapeQuotes(defaultValue)}'`;
-		} else if (type === 'XML') {
-			return `CAST(N'${defaultValue}' AS xml)`;
+		if (type === 'XML') {
+			return `CAST(N'${escapeQuotes(defaultValue)}' AS xml)`;
 		}
 
 		return defaultValue;
