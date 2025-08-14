76102eb90f7aeb3a3f3eaff0e4b8bb43e1f7b69b
yevhenii moroziuk <yevhen.moroziuk@hackolade.com>
2025-08-14 16:37:25 +0300

    HCK-12377: Simplify the code

--- forward_engineering/utils/general.js
+++ forward_engineering/utils/general.js
@@ -166,15 +166,16 @@ module.exports = _ => {
 		return name.replace(/'/g, "''");
 	};
 
-	const skipSqlCommentsPattern = /^\s*(EXEC(UTE)?\b|.*\bMS_DESCRIPTION\b)/i;
+	const skipSqlCommentsPattern = /^\s*(EXEC\b|.*\bMS_DESCRIPTION\b)/i;
 
 	const buildScript = statements => {
-		const formattedScripts = statements.filter(Boolean).map(script => {
-			if (skipSqlCommentsPattern.test(script)) {
-				return script;
-			}
-			return sqlFormatter.format(script, { indent: '    ' }).replace(/\{ \{ (.+?) } }/g, '{{$1}}');
-		});
+		const formattedScripts = statements
+			.filter(Boolean)
+			.map(script =>
+				skipSqlCommentsPattern.test(script)
+					? script
+					: sqlFormatter.format(script, { indent: '    ' }).replace(/\{ \{ (.+?) } }/g, '{{$1}}'),
+			);
 
 		return formattedScripts.join('\n\n') + '\n\n';
 	};
